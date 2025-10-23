2ef184da138ef5a674723417945703f0353806d0
yoseftaktuk <taktuk07@gmail.com>
2025-10-23 09:46:11 +0300

    Adding functions

---
+++ game_logic/game.py
@@ -0,0 +1,9 @@
+def create_player(name:str) -> dict:
+    pass
+
+
+def nit_game()->dict:
+    pass
+
+def play_round(p1:dict,p2:dict):
+    pass
---
+++ main.py
@@ -0,0 +1,3 @@
+from game_logic import game
+if __name__ == "__main__":
+    pass
