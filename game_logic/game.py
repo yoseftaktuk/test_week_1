54eaf6751eccbba5f53823832d0dad85eafc88d0
yoseftaktuk <taktuk07@gmail.com>
2025-10-23 12:30:49 +0300

    finish game.py and main.py

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,10 +1,42 @@
-def create_player(name:str):
-    return {}
+from utils import deck
+
+def create_player(*name:str):
+    player_name = name
+    if len(name) == 0:
+        player_name = 'AI'
+    player = {'name': player_name,'hand':[],'won_pile':[]}
+    return player
 
 
 
-def nit_game():
-    return {}
+def init_game():
+    player_1 = create_player('yosef')
+    player_2 = create_player()
+    new_deck = deck.shuffle(deck.create_deck())
+    player_1['hand'] = new_deck[0:26]
+    player_2['hand'] = new_deck[26:]
+    return {'dect':new_deck,
+            'player_1':player_1,
+            'player_2':player_2}
 
 def play_round(p1:dict,p2:dict):
-    pass
+    hand_1 = p1['hand'].pop()
+    hand_2 = p2['hand'].pop()
+    if hand_1['value'] > hand_2['value']:
+            p1['won_pile'].append(hand_1)
+            p1['won_pile'].append(hand_2)
+            print('hand 1 won!!!')
+    elif hand_1['value'] == hand_2['value']:
+        print('Its a draw')
+    else:
+        p2['won_pile'].append(hand_1)
+        p2['won_pile'].append(hand_2)
+        print('hand 2 won!!!')
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from game_logic import game
 if __name__ == "__main__":
-    pass
+    from  game_logic import game
+
+    a = game.create_player('yosef')
+    b = game.init_game()
+    c = game.init_game()
+    if len(c['player_1']['hand']) or len(c['player_2']['hand']) == 0:
+        print('game over')
+    else:
+        game.play_round(c['player_1'],c['player_2'])
+        game.play_round(c['player_1'],c['player_2'])
+        game.play_round(c['player_1'],c['player_2'])
+        game.play_round(c['player_1'],c['player_2'])
